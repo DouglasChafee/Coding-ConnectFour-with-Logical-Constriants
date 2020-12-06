809263e299e7c4dd7e0068418264cb4124112ed8
KarlDorogy <70972627+KarlDorogy@users.noreply.github.com>
2020-12-05 19:57:47 -0500

    Basis of Connect Four Board

--- run.py
+++ run.py
@@ -1,17 +1,101 @@
-from nnf import Var
+
+from nnf import Var,true
 from lib204 import Encoding
 
+from nnf import NNF
+from nnf.operators import iff
+
+def implication(l, r):
+    return l.negate() | r
+
+def neg(f):
+    return f.negate()
+
+NNF.__rshift__ = implication
+NNF.__invert__ = neg
+
+#variables to be turned into constraints
 C = Var('A winning column')
 L = Var("Black's turn")
 J = Var("Red's turn")
-R = Var("Red unit")
-B = Var('Black unit')
 H = Var("A winning row")
 Z = Var("This value is true when x is zero")
 W = Var("A winning game")
 U = Var("An occupied slot")
 P = Var("if either black or red has the option to win in one turn")
 D = Var("A winning diagonal")
+
+# Creating ConnectFour Game Board
+rowNum = 6
+columnNum = 7
+
+# Creating boards for all possibilities of that a single position could be, black, red, or empty
+blackBoard=[]
+redBoard=[]
+emptyBoard=[] 
+for i in range(rowNum): 
+    blackBoard.append([])
+    redBoard.append([])
+    emptyBoard.append([])
+    for j in range(columnNum):
+        blackBoard[i].append(Var(f"Black({i},{j})"))
+        redBoard[i].append(Var(f"Red({i},{j})"))
+        emptyBoard[i].append(Var(f"Empty({i},{j})"))
+# Printing example of Black connect four Game Board
+for row in blackBoard:
+  print(row)
+
+# Prints a Connect Four board
+def printBoard(dic):
+  board=[]
+  for i in range(rowNum): 
+    board.append([])
+    for j in range(columnNum):
+      board[i].append("N")
+  if dic == None:
+    print("NonSatisfiable Board")
+    return []
+  else:
+    for key, value in dic.items():
+      if (key[:6] == "Black(") and (value == True):
+        xVal = int(key[-4])
+        yVal = int(key[-2])
+        board[xVal][yVal] = "B"
+      elif (key[:4] == "Red(") and (value == True):
+        xVal = int(key[-4])
+        yVal = int(key[-2])
+        board[xVal][yVal] = "R"
+
+  for row in board:
+    print(row)
+        
+
+
+def gravityRule(i, j):
+  f = true
+  for slot in range(i + 1, rowNum):
+    f &= ~emptyBoard[slot][j]
+  return f
+  # return ~emptyboard
+  
+
+
+
+# Holds constraints/rules that make up a valid connect four board.
+def validBoard(E):
+  for i in range(rowNum):
+    for j in range(columnNum): 
+      # If position(i, j) is empty, then neither black or red can occupy position(i, j).
+      E.add_constraint(emptyBoard[i][j] >> (~redBoard[i][j] & ~blackBoard[i][j]))
+
+      # If position(i, j) is red, then neither black or empty can occupy position(i, j) and 
+      # there must be non # empy positions below position(i,j) (gravity).
+      E.add_constraint(redBoard[i][j] >> (~blackBoard[i][j] & ~emptyBoard[i][j] & gravityRule(i, j)))
+      E.add_constraint(blackBoard[i][j] >> (~redBoard[i][j] & ~emptyBoard[i][j] & gravityRule(i, j)))
+
+      # make sure implication works properly above, at least one has to be true.
+      E.add_constraint(emptyBoard[i][j] | redBoard[i][j] | blackBoard[i][j])
+  return E
 
 
 
@@ -22,55 +106,33 @@ D = Var("A winning diagonal")
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
 
-def connectFour_validWin():
-    T = Encoding()
-    T.add_constraint(~L | ~J)
-    T.add_constraint(~B | ~R)
-    T.add_constraint(B | R | Z)
-    T.add_constraint(B | R | (~B & ~R))
-    T.add_constraint(~H | ((B&B&B&B)|(R&R&R&R)))
-    T.add_constraint(~C | ((B&B&B&B)|(R&R&R&R)))
-    T.add_constraint(~P | (~U&B&B&B)|(B&~U&B&B)|(B&B&~U&B)|(B&B&B&~U)|(~U&R&R&R)|(R&~U&R&R)|(R&R&~U&R)|(R&R&R&~U))
-    T.add_constraint(~W | (H | C | D))
-    return T
+def connectFour():
+    E = Encoding()
+    E = validBoard(E)
+    #add winning function with constraints like validBoard to see if board is a won state.
 
-def connectFour_blackWin():
-    T = Encoding()
-    T.add_constraint(~L | ~J)
-    T.add_constraint(~B | ~R)
-    T.add_constraint(B | R | Z)
-    T.add_constraint(B | R | (~B & ~R))
-    T.add_constraint(~H | ((B&B&B&B)|(R&R&R&R)))
-    T.add_constraint(~C | ((B&B&B&B)|(R&R&R&R)))
-    T.add_constraint(~P | (~U&B&B&B)|(B&~U&B&B)|(B&B&~U&B)|(B&B&B&~U)|(~U&R&R&R)|(R&~U&R&R)|(R&R&~U&R)|(R&R&R&~U))
-    T.add_constraint(~W)
-    T.add_constraint(B&B&B)
-    return T
+    #E.add_constraint(emptyBoard[0][0])
+    #E.add_constraint(~emptyBoard[0][0])
 
-def connectFour_redWin():
-    T = Encoding()
-    T.add_constraint(~L | ~J)
-    T.add_constraint(~B | ~R)
-    T.add_constraint(B | R | Z)
-    T.add_constraint(B | R | (~B & ~R))
-    T.add_constraint(~H | ((B&B&B&B)|(R&R&R&R)))
-    T.add_constraint(~C | ((B&B&B&B)|(R&R&R&R)))
-    T.add_constraint(~P | (~U&B&B&B)|(B&~U&B&B)|(B&B&~U&B)|(B&B&B&~U)|(~U&R&R&R)|(R&~U&R&R)|(R&R&~U&R)|(R&R&R&~U))
-    T.add_constraint(~W)
-    T.add_constraint(R&R&R)
-    return T
 
+    return E
 
 
 if __name__ == "__main__":
 
-    T = connectFour_validWin()
+    E = connectFour()
 
-    print("\nSatisfiable: %s" % T.is_satisfiable())
-    print("# Solutions: %d" % T.count_solutions())
-    print("   Solution: %s" % T.solve())
+    print("\nSatisfiable: %s" % E.is_satisfiable())
+    #print("# Solutions: %d" % E.count_solutions())
+    dic = E.solve()
+    print("   Solution: %s" % dic)
 
-    print("\nVariable likelihoods:")
-    for v,vn in zip([P,W,H,C,D], 'PWHCD'):
-        print(" %s: %.2f" % (v, T.likelihood(vn)))
-    print()
+    #Tostring function basically that takes dictionary output of E.solve and prints a connect four board
+    printBoard(dic)
+    
+
+
+    # print("\nVariable likelihoods:")
+    # for v,vn in zip([P,W,H,C,D], 'PWHCD'):
+    #     print(" %s: %.2f" % (v, T.likelihood(vn)))
+    # print()
